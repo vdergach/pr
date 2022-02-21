5e549d1626dc2453e91d4f790d3533feca9a449a
user <password>
2022-02-21 10:46:41 +0200

    commit via mobile v3

--- src/test/java/OpenWebPage.java
+++ src/test/java/OpenWebPage.java
@@ -9,16 +9,13 @@ public class OpenWebPage {
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\user\\Downloads\\chromedriver_win32\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
 
-        driver.get("http://www.google.com/");
         driver.manage().window().maximize();
-//123
+        Thread.sleep(3000);
+        driver.get("http://www.google.com/");
         WebElement searchBox = driver.findElement(By.name("q"));
-        Thread.sleep(2000);
         searchBox.sendKeys("It Works!!!");
         searchBox.submit();
-        Thread.sleep(2000);
         WebElement webSiteName = driver.findElement(By.xpath("/html/body/div[7]/div/div[10]/div/div[2]/div[2]/div/div/div[2]/div/div/div[1]/div/a/h3"));
-        Thread.sleep(2000);
         Assert.assertEquals(webSiteName.getText(),"It Works!");
 //123
         driver.quit();
